e263f18faa005f88b179f38fcaa979ba7199e9be
Danny <dannypacheco1990@gmail.com>
2014-04-18 10:35:42 +0200

    u

--- Deg(resto)/index.php
+++ Deg(resto)/index.php
@@ -1,16 +1,16 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 session_start();
-$pagefiles = array("home.php","login");
-$pageU = $_GET["page"].".php";
-$page = "apps/home.php";
+$pagefiles = array("home","login","menu","livre_or","coordonnee","command");
+$pageU = $_GET["page"];
+$page = "apps/home";
 if(isset($_GET['page'])){
 	if (in_array($pageU,$pagefiles))
 	{
 		$page = "apps/".$pageU;
 	}
 	else{
-		$page = "apps/404.php";
+		$page = "apps/404";
 	}
 }
 else
