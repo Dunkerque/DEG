c60f4b9d0f948d5aaca305055b12ad7437a133ef
Danny <dannypacheco1990@gmail.com>
2014-04-18 17:08:33 +0200

    u

--- Deg(resto)/index.php
+++ Deg(resto)/index.php
@@ -2,21 +2,27 @@
 header('Content-Type: text/html; charset=utf-8');
 $db = mysqli_connect("localhost","root","troiswa","restodeg");
 /*| Il faut vérifier si la connection mysql a reussi ! |*/
-session_start();
-$pagefiles = array("home","menu","livre_or","coordonnee","command","logout", "register","admin","profile");
-$pageU = $_GET["page"];
-$page = "apps/home";
-if(isset($pageU)){
-	if (in_array(strtolower($pageU),$pagefiles))
-	{
-		$page = "apps/".$pageU;
+if($db)
+{
+	session_start();
+	$pagefiles = array("home","menu","livre_or","coordonnee","command","logout", "register","admin","profile");
+	$pageU = $_GET["page"];
+	$page = "apps/home";
+	if(isset($pageU)){
+		if (in_array(strtolower($pageU),$pagefiles))
+		{
+			$page = "apps/".$pageU;
+		}
+		else{
+			$page = "apps/404";
+		}
 	}
-	else{
-		$page = "apps/404";
-	}
+	else
+	    header('location:index.php?page=home');
+	require("apps/skel.php");
+	
 }
-else
-    header('location:index.php?page=home');
-require("apps/skel.php");
-
+else{
+	die(mysqli_connect_error());
+}
 ?>
