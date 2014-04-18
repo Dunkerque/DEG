a9b5e34a297a5e53b22a69a8012c1a262da6c703
Danny <dannypacheco1990@gmail.com>
2014-04-18 09:45:44 +0200

    Update index.php

--- Deg(resto)/index.php
+++ Deg(resto)/index.php
@@ -1,13 +1,18 @@
 <?php
-
+header('Content-Type: text/html; charset=utf-8');
+session_start();
 $pagefiles = array("home.php");
 $pageU = $_GET["page"].".php";
 
-if (in_array($pageU,$pagefiles))
-{
-	$page = "apps/".$pageU;
+if(isset($_GET['page'])){
+	if (in_array($pageU,$pagefiles))
+	{
+		$page = "apps/".$pageU;
+	}
+	else{
+		$page = "apps/404.php";
+	}
 }
-
 require("apps/skel.php");
 
 ?>
