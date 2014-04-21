f6c94d5a44fa56758ba12303a35cc677e490dd71
Dunkerque <Dannypacheco1990@gmail.com>
2014-04-21 22:44:37 +0200

    update

--- apps/modules/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	// JE RECUPERE LURL EN COURS (index.php?page=module)
-	$url_en_cours = basename($_SERVER['REQUEST_URI']);
-
-	// J'EXPLODE MA CHAINE APRES le index.php? POUR RECUPERE QUE LE MODULE EN COURS;
-	$url = explode("?", basename(trim($_SERVER['REQUEST_URI'])));
-	$url = $url[1];
-	// $encours[1] = page=lemodule
-?>
--- apps/modules/profile_admin.php
+++
@@ -1,4 +0,0 @@
-<?php
-if ($resUser["admin"] == 1)
-	require("views/profile_admin.html");
-?>
