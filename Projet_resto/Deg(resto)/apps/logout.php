64274afe2b33a455e231f442bd37ab66fb480b11
Dunkerque <Dannypacheco1990@gmail.com>
2014-04-21 22:52:47 +0200

    update

--- Projet_resto/Deg(resto)/apps/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$_SESSION = array();
-session_destroy();
-/*| Faites pas de Location:index.php si c'est pour refaire un Location:index.php?page=home juste apres... :) |*/
-header('Location:index.php');
-?>
