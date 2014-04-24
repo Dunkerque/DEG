42abe64fb4c57cf3f4c501eb882a8bc75d8e4e3c
hearsute <manuoval@gmail.com>
2014-04-24 11:48:40 +0200

    yeah

--- apps/display_edit_users.php
+++ apps/display_edit_users.php
@@ -13,8 +13,16 @@ $resQuerySUsers = mysqli_query($db,$querySUsers);
 
 while($resSUsers = mysqli_fetch_assoc($resQuerySUsers))
 {
+	$idSU = $resSUsers["id_users"];
+
     $loginSU = htmlentities($resSUsers["login"]);
-    $idSU = $resSUsers["id_users"];
+    $nameSU = htmlentities($resSUsers["nom"]);
+    $surnameSU = htmlentities($resSUsers["prenom"]);
+    $emailSU = htmlentities($resSUsers["email"]);
+    $adressSU = htmlentities($resSUsers["adresse"]);
+    $cpSU = htmlentities($resSUsers["email"]);
+    $citySU = htmlentities($resSUsers["ville"]);
+    $infosSU = htmlentities($resSUsers["info_complementaire"]);
     $registerDateSU = $resSUsers["register_date"];
     require("views/display_edit_users.html");
 }
