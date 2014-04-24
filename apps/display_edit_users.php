81c94ed9fca1465800895a01ceecf53706e81231
hearsute <manuoval@gmail.com>
2014-04-24 10:54:31 +0200

    yeah

--- apps/display_edit_users.php
+++ apps/display_edit_users.php
@@ -6,6 +6,7 @@ $resQuerySUsers = mysqli_query($db,$querySUsers);
 while($resSUsers = mysqli_fetch_assoc($resQuerySUsers))
 {
     $loginSU = htmlentities($resSUsers["login"]);
+    $idSU = $resSUsers["id_users"];
     $registerDateSU = $resSUsers["register_date"];
     require("views/display_edit_users.html");
 }
