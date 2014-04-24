0401df57307c09610deb1f47ee6fa38f9b530844
eBabon <ericbabon@gmail.com>
2014-04-24 14:04:47 +0200

    update

--- apps/modules/scroll_plats.php
+++ apps/modules/scroll_plats.php
@@ -1,18 +1,13 @@
 <?php 
-	// Danny style:
-	// $showAll = "SELECT * FROM plats";
-	// $res = mysqli_query($db, $showAll);
-	// while($data = mysqli_fetch_assoc($res))
-	// {
-	// 	$namePlats = $data['nom_plats'];
-	// 	//var_dump($data);
-	// }
-
-	$display_plats = mysqli_query(SELECT nom_plats FROM plats); 
-	while ($res = mysqli_fetch_array(display_plats)) {
-		# code...
+	
+	$showAll = "SELECT * FROM plats";
+	$res = mysqli_query($db, $showAll);
+	$select_open = '<select name="contenu_menu" value="<?= $edit_contenu_menu; ?>">';
+	while($data = mysqli_fetch_assoc($res))
+	{
+		$namePlats = $data['nom_plats'];
 	}
-	$namePlats='';	
-
-	require("views/scroll_plats.html");	
+	$select_end = "</select>";
+	require("views/scroll_plats.html");
+	
 ?>
