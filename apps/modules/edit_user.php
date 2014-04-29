ecbf74f6816f79a624d5a41a0ef7bdb5969fac35
hearsute <manuoval@gmail.com>
2014-04-29 12:11:19 +0200

    yeah

--- apps/modules/edit_user.php
+++ apps/modules/edit_user.php
@@ -11,46 +11,42 @@ if (isset($_SESSION["login"]))
     {
         if ($_SESSION["admin"] === 1)
         {
-
             $querySUser = 'SELECT * FROM users WHERE id_users = "'.$idUser.'"';
             $resQuerySUser = mysqli_query($db,$querySUser);
             $resSUser = mysqli_fetch_object($resQuerySUser, "User"); //on fait un retour sous forme d'objet. Les variables privées de la classe sont remplies automatiquement. Ici on peut afficher les infos remplies auto avec les GETTERS. voir fichier user_class.php
-
             if (isset($_POST["update_user_sub"]))
             {
-                $userEdit = new User();
+                $resSUser->setIdUser($idUser);
+                $resSUser->setLogin($_POST["update_login"]);
+                $resSUser->setName($_POST["update_name"]);
+                $resSUser->setSurname($_POST["update_surname"]);
+                $resSUser->setEmail($_POST["update_email"]);
+                $resSUser->setAdress($_POST["update_adress"]);
+                $resSUser->setCp($_POST["update_cp"]);
+                $resSUser->setCity($_POST["update_city"]);
+                $resSUser->setInfos($_POST["update_info_comp"]);
 
-                $userEdit->setIdUser($idUser);
-                $userEdit->setLogin($_POST["update_login"]);
-                $userEdit->setName($_POST["update_name"]);
-                $userEdit->setSurname($_POST["update_surname"]);
-                $userEdit->setEmail($_POST["update_email"]);
-                $userEdit->setAdress($_POST["update_adress"]);
-                $userEdit->setCp($_POST["update_cp"]);
-                $userEdit->setCity($_POST["update_city"]);
-                $userEdit->setInfos($_POST["update_info_comp"]);
-
-                $queryEditUser = 'UPDATE users SET login = "'.$userEdit->getLogin().'",
-                nom = "'.$userEdit->getName().'",
-                prenom = "'.$userEdit->getSurname().'",
-                email = "'.$userEdit->getEmail().'",
-                adresse = "'.$userEdit->getAdress().'" ,
-                code_postal = "'.$userEdit->getCp().'",
-                ville = "'.$userEdit->getCity().'",
-                info_complementaire = "'.$userEdit->getInfos().'" WHERE id_users = "'.$userEdit->getIdUser().'" ';
+                $queryEditUser = 'UPDATE users SET 
+                login = "'.mysqli_real_escape_string($db,$resSUser->getLogin()).'",
+                nom = "'.mysqli_real_escape_string($db,$resSUser->getName()).'",
+                prenom = "'.mysqli_real_escape_string($db,$resSUser->getSurname()).'",
+                email = "'.mysqli_real_escape_string($db,$resSUser->getEmail()).'",
+                adresse = "'.mysqli_real_escape_string($db,$resSUser->getAdress()).'" ,
+                code_postal = "'.mysqli_real_escape_string($db,$resSUser->getCp()).'",
+                ville = "'.mysqli_real_escape_string($db,$resSUser->getCity()).'",
+                info_complementaire = "'.mysqli_real_escape_string($db,$resSUser->getInfos()).'" WHERE id_users = "'.$resSUser->getIdUser().'" ';
                 $resQueryEditUser = mysqli_query($db,$queryEditUser);
 
-                if ($userEdit->getError() === null)
+                if ($resSUser->getError() === null)
                 {
                     $msg = "Le profil à bien été modifié";
                 }
 
                 else
                 {
-                    $msg = $userEdit->getError();
+                    $msg = $resSUser->getError();
                 }
             }
-
             require("views/edit_user.html");
         }
 
