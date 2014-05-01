88bf3c891a7087b8c2420775d6d1bb00be046bff
Dunkerque <Dannypacheco1990@gmail.com>
2014-05-01 20:58:02 +0200

    u

--- apps/config.php
+++ apps/config.php
@@ -2,7 +2,6 @@
 	// JE RECUPERE LURL EN COURS (index.php?page=module)
 if(isset($_GET['page']))
 {
-	$url_en_cours = basename($_SERVER['REQUEST_URI']);
 	// J'EXPLODE MA CHAINE APRES le index.php? POUR RECUPERE QUE LE MODULE EN COURS;
 	$url = explode("?", basename(trim($_SERVER['REQUEST_URI'])));
 	$url = $url[1];
