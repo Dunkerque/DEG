c5da6cf9b6fe3f6d8f7e4517445ed26300d71c4a
Danny <dannypacheco1990@gmail.com>
2014-05-02 09:35:33 +0200

    u

--- apps/display_plats.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$showPlats = "SELECT * FROM plats";	
-	$resPlats = mysqli_query($db, $showPlats);
-	while($dataPlats = mysqli_fetch_assoc($resPlats))
-	{
-		$namePlats = htmlentities($dataPlats['nom_plats']);
-		echo '<option value="'.$dataPlats['nom_plats'].'">'.$namePlats.'</option>';
-	}
-?>
